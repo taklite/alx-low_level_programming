01c9d05a5a7a55fbf163414e878fd5e085bced44
taklite <waleedtaklite@hotmail.com>
2023-08-02 11:36:11 -0700

    adding task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,13 @@
  */
 
 void _puts_recursion(char *s)
+{
+	if (*s)
 	{
-		if (*s)
-		{
-			_putchar(*s);
-			_puts_recursion(s + 1);
-		}
-		else
-		{
-			_putchar('\n');
-		}
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+
+	else
+		_putchar('\n');
+}

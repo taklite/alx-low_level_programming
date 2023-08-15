761460878b59248c5653324cb5c803c57291e073
taklite <waleedtaklite@hotmail.com>
2023-08-14 17:53:56 -0700

    fixing task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,7 +9,6 @@
  *
  * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
@@ -20,13 +19,13 @@ struct dog
 /**
  * dog_t - typedef for struct dog
  */
-	typedef struct dog dog_t;
+typedef struct dog dog_t;
 
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
